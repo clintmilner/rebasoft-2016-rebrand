4663e0c9dfaadca1c4fe3f788f7c32418768cf8b
Clint Milner <clinton.milner@gmail.com>
2015-12-18 14:30:58 +0000

    getting header and slider styled up

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,46 @@
  *
  * Rebasoft - Network Intelligence
  */
+
+var gulp = require( 'gulp' );
+var less = require( 'gulp-less' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+var minifyCss = require( 'gulp-minify-css' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+
+var masterLess = 'builder/less/master.less';
+var cssTarget = 'static/css';
+var jsTarget = 'static/js';
+
+var bsLess = [ 'builder/less/*.less' ],
+    bsJS = [ 'builder/js/tooltip.js', 'builder/js/*.js' ]; // tooltip.js needs to be loaded first
+
+    gulp.task('build-less', function() {
+        gulp.src(masterLess)
+            .pipe(less())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(minifyCss({keepBreaks: true}))
+            .pipe(gulp.dest(cssTarget));
+
+        console.log('Finished compiling LESS files to ' + cssTarget);
+    });
+
+    gulp.task( 'build-js', function(){
+        gulp.src( bsJS )
+            .pipe( concat( 'scripts.js' ) )
+
+            .pipe( gulp.dest( jsTarget ) );
+
+        console.log( 'Finished compiling Bootstrap JS' );
+    });
+
+    gulp.task( 'watch', function(){
+       gulp.watch( bsLess, [ 'build-less' ] );
+       gulp.watch( bsJS, [ 'build-js' ] );
+    });
+
+    gulp.task( 'default', ['build-less', 'build-js', 'watch'] );
