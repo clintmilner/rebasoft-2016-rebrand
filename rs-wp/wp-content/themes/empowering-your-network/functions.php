df0d8954d3f7680be209561473cea4f435946205
Clint Milner <clinton.milner@gmail.com>
2015-12-23 13:01:14 +0000

    twitter widget area / plugin / styling

--- rs-wp/wp-content/themes/empowering-your-network/functions.php
+++ rs-wp/wp-content/themes/empowering-your-network/functions.php
@@ -111,6 +111,15 @@ function empowering_your_network_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => 'Home Page Twitter Feed',
+		'id'            => 'div-rs-twitter-home',
+//		'before_widget' => '<div>',
+//		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="hidden">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'empowering_your_network_widgets_init' );
 
