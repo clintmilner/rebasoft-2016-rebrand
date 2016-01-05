cf692e6ef31ab74e6a5bfe8488c03cfed497bc53
Clint Milner <clinton.milner@gmail.com>
2016-01-05 10:41:02 +0000

    standard page template fixes

--- rs-wp/wp-content/themes/empowering-your-network/page.php
+++ rs-wp/wp-content/themes/empowering-your-network/page.php
@@ -7,32 +7,36 @@
  * and that other 'pages' on your WordPress site may use a
  * different template.
  *
+ *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Rebasoft
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="container-fluid rs-feed">
+	<div id="rs-sub-header" class="row">
+		<div class="col-xs-12">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</div>
 
-			<?php
-			while ( have_posts() ) : the_post();
+	<div id="rs-main-content-top" class="row rs-main-content rs-team">
+		<div class="col-sm-12 col-md-7">
+
+			<?php while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			endwhile; ?>
+		</div>
 
-			endwhile; // End of the loop.
-			?>
+		<?php get_template_part( '/template-parts/content', 'contactform' ); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div>
 
-<?php
-get_sidebar();
-get_footer();
+	<?php get_footer(); ?>
+
+</div>
